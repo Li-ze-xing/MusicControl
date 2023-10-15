f62eac89fc6993a58a280cd7e516b8951c7098a6
inia123 <1960116542@qq.com>
2023-10-15 15:36:15 +0800

    yzh

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, re_path
 
+from app import views
+from app.views import Charts
+
 urlpatterns = [
-    
+    path('charts/', Charts.as_view(), name='charts'),
 ]
                                 
--- app/views.py
+++ app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import User,MusicList
+from django.views.generic import View
+from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+
+class Charts(View):
+    """类视图：处理排行榜"""
+    def get(self, request):
+        """处理GET请求，返回票数页面"""
+        data = {'c': MusicList.objects.all().order_by('-callNum')}
+        print(data)
+        return render(request, 'charts.html', data)
+
+    def post(self, request):
+        """处理POST请求"""
+        pass
+        return HttpResponse("POST")
+
+
